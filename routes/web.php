5a19be80fb2d907ac9d9f976d09e76d02946c42f
evandrarf <evandraraditya049@gmail.com>
2022-12-11 18:19:56 +0700

    resource-controller-and-route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,12 +24,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get("/students", [StudentController::class, "index"])->name("students.index");
-Route::get("/students/create", [StudentController::class, "create"])->name("students.create");
-Route::post("/students", [StudentController::class, "store"])->name("students.store");
-Route::get("/students/{id}/edit", [StudentController::class, "edit"])->name("students.edit");
-Route::put("/students/{id}", [StudentController::class, "update"])->name("students.update");
-Route::delete("/students/{id}", [StudentController::class, "destroy"])->name("students.destroy");
+// Route::get("/students", [StudentController::class, "index"])->name("students.index");
+// Route::get("/students/create", [StudentController::class, "create"])->name("students.create");
+// Route::post("/students", [StudentController::class, "store"])->name("students.store");
+// Route::get("/students/{id}/edit", [StudentController::class, "edit"])->name("students.edit");
+// Route::put("/students/{id}", [StudentController::class, "update"])->name("students.update");
+// Route::delete("/students/{id}", [StudentController::class, "destroy"])->name("students.destroy");
+
+Route::resource('students', StudentController::class);
 
 
 Route::middleware('auth')->group(function () {
